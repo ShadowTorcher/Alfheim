8faa27a8e7b1d0d57f9c0caa70833e21a52e425d
Starxy <galaxy_djx@hotmail.com>
2018-09-10 12:29:07 +0800

    first commit

---
+++ src/main/java/club/shadowtorcher/alfheim/AlfheimApplication.java
@@ -0,0 +1,12 @@
+package club.shadowtorcher.alfheim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfheimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfheimApplication.class, args);
+    }
+}
